e5f027187a45edecea07d9620577c44ea53831b6
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2026-01-06 01:59:43 -0300

    feat: adiciona método findByCategories_Id no repositório de filmes

--- src/main/java/com/movieflix/repository/MovieRepository.java
+++ src/main/java/com/movieflix/repository/MovieRepository.java
@@ -4,6 +4,10 @@ import com.movieflix.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MovieRepository extends JpaRepository<Movie,Long> {
+
+    List<Movie> findByCategories_Id (Long categoryId);
 }
