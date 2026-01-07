7d78fad8e6e6d0eafce34c2b80113a1f97628495
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2026-01-07 00:04:19 -0300

    feat: adiciona consulta de usuário por email no repositório

--- src/main/java/com/movieflix/repository/UserRepository.java
+++ src/main/java/com/movieflix/repository/UserRepository.java
@@ -1,9 +1,14 @@
-package com.movieflix.repository;
+    package com.movieflix.repository;
 
-import com.movieflix.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+    import com.movieflix.entity.User;
+    import org.springframework.data.jpa.repository.JpaRepository;
+    import org.springframework.security.core.userdetails.UserDetails;
+    import org.springframework.stereotype.Repository;
 
-@Repository
-public interface UserRepository extends JpaRepository<User,Long> {
-}
+    import java.util.Optional;
+
+    @Repository
+    public interface UserRepository extends JpaRepository<User,Long> {
+
+        Optional<UserDetails> findUserByEmail(String email);
+    }
