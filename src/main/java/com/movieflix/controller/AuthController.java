fcf89110084e8b39382abb038116b049c196052a
ghenriqf <gabrielhenriquefreitas28@gmail.com>
2026-01-06 21:10:34 -0300

    feat: adiciona endpoint de registro de usuário no controlador de autenticação

---
+++ src/main/java/com/movieflix/controller/AuthController.java
@@ -0,0 +1,25 @@
+package com.movieflix.controller;
+
+import com.movieflix.controller.request.UserRequest;
+import com.movieflix.controller.response.UserResponse;
+import com.movieflix.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/movieflix/auth")
+public class AuthController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse> register (@RequestBody UserRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(request));
+    }
+}
